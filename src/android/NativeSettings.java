2941aa0bd3712116908190cb84e6a62fdc4761a1
Nicolas Thomas <n.thomas@waigeo.fr>
2019-05-15 17:07:20 +0200

    fixed action7

--- src/android/NativeSettings.java
+++ src/android/NativeSettings.java
@@ -14,6 +14,7 @@ import org.json.JSONException;
 import android.content.Intent;
 import android.content.Context;
 import android.net.Uri;
+import android.content.ComponentName;
 
 import android.provider.Settings;
 
